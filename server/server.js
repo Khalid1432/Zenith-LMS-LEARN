5c6c4774795d549b11d89469756de72f6846f3c3
Khalid1432 <khalidjameel145@gmail.com>
2025-08-03 03:12:38 +0530

    initial

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 const allowedOrigins = [
-  "http://localhost:5173",
   "https://zenith-lms-learn.vercel.app"
 ];
 
