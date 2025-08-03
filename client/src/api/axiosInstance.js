567901834b89fefea797b1bab69f53247f60a4f2
Khalid Jameel <139610224+Khalid1432@users.noreply.github.com>
2025-08-03 12:33:31 +0530

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://zenith-lms-learn-backend.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
